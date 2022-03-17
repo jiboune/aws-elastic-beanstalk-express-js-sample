de627699f83eeb4d449603e358da0edf8da51e81
jiboune <jeanbaptiste@enovap.com>
2022-03-17 14:30:44 +0100

    Ajout route jb

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello jibz'));
+app.get('/', (req, res) => res.send('Hello AWS'));
+app.get('/jb', (req, res) => res.send('Hello jibz AWS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
